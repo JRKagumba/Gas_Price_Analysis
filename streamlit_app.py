6a085c2a305fc69b2a6eb933182f1f9c496d3e0b
JRKagumba <63820705+JRKagumba@users.noreply.github.com>
2022-03-15 09:39:45 -0400

    Remove code that was commented out

--- streamlit_app.py
+++ streamlit_app.py
@@ -335,59 +335,10 @@ st.markdown("* With increaseing options of non-fuel based transportation alterna
 ########################################################################################################################
 
 
-# st.markdown('***')
-
-# from utils import db
-# from datetime import datetime
-
-# COMMENT_TEMPLATE_MD = """{} - {}
-# > {}"""
-
-# def space(num_lines=1):
-#     """Adds empty lines to the Streamlit app."""
-#     for _ in range(num_lines):
-#         st.write("")
-
-# # Comments part
-
-# conn = db.connect()
-# comments = db.collect(conn)
-
-# with st.expander("💬 Open comments"):
-#     # Show comments
-#     st.write("**Comments:**")
-
-#     for index, entry in enumerate(comments.itertuples()):
-#         st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
-
-#         is_last = index == len(comments) - 1
-#         is_new = "just_posted" in st.session_state and is_last
-#         if is_new:
-#             st.success("Your comment was successfully posted.")
-
-#     space(2)
-
-#     # Insert comment
-
-#     st.write("**Add your own comment:**")
-#     form = st.form("comment")
-#     name = form.text_input("Name")
-#     comment = form.text_area("Comment")
-#     submit = form.form_submit_button("Add comment")
-
-#     if submit:
-#         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-#         db.insert(conn, [[name, comment, date]])
-#         if "just_posted" not in st.session_state:
-#             st.session_state["just_posted"] = True
-#         st.experimental_rerun()
-
-
-
 st.markdown('***')
 
 st.header("Contacts")
-# [![Twitter Follow](https://img.shields.io/twitter/follow/PogrebnyakE?label=Follow&style=social)](https://twitter.com/PogrebnyakE)
+
 """
 [![MAIL Badge](https://img.shields.io/badge/-jrkagumba@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:e.pogrebnyak@gmail.com)](mailto:e.pogrebnyak@gmail.com)
 \n
